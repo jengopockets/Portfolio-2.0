fb41912ad8dc8074a8dcd6de14508f899d667952
Jengopockets <jengopockets@gmail.com>
2019-10-13 03:40:40 -0400

    Created Star Animation

---
+++ src/components/Main/Title.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <div id="title">
+    <h1>JengoDev</h1>
+    <h3>by</h3>
+    <h2>Jacob Tharp</h2>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "title",
+}
+</script>
+
+<style scoped>
+h1 {
+    opacity: 1;     
+}
+div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+}
+#title {   
+    color: white; 
+    border: 2px solid rgb(5, 3, 32);
+    opacity: 1;
+}
+</style>
---
+++ src/components/Main/TitleContainer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <Title />
+    <Profile />
+    </div>
+</template>
+<script>
+import Title from "./Title";
+import Profile from "./Profile";
+export default {
+    name: "titleContainer",  
+    components: {
+        Title,
+        Profile
+    }  
+}
+</script>
+<style scoped>
+div {
+    margin-right: 33%;
+    margin-left: 33%;
+    display: flex;
+}
+</style>
